51c2791f89adc8cd7b3661eec10814f8a132e9f1
diandrakina <diandrapramestikinasih@gmail.com>
2024-01-03 13:40:10 +0700

    diandraPK

--- lib/pages/login_page/loginPage.dart
+++ lib/pages/login_page/loginPage.dart
@@ -1,1 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:selamat_application/styles/styles.dart';
+import 'package:selamat_application/widget/customTextFormField.dart';
 
+class loginPage extends StatelessWidget {
+  loginPage({super.key});
+
+  TextEditingController userNameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Form(
+          child: Container(
+        width: double.maxFinite,
+        padding: EdgeInsets.symmetric(vertical: 33),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Spacer(),
+            Text(
+              'Selamat',
+              style: TextStyles.GR_40_title_regular,
+            ),
+            Text(
+              'Sehat Selalu Mentalku',
+              style: TextStyles.GR_24_title_light,
+            ),
+            Text('halo'),
+            SizedBox(
+              height: 38,
+            ),
+            _buildUserName(context),
+            SizedBox(
+              height: 26,
+            ),
+            _buildPassword(context),
+            SizedBox(
+              height: 15,
+            ),
+          ],
+        ),
+      )),
+    ));
+  }
+
+  Widget _buildUserName(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      child: CustomTextFormField(
+        controller: userNameController,
+        hintText: 'username',
+      ),
+    );
+  }
+
+  Widget _buildPassword(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.symmetric(horizontal: 50),
+        child: CustomTextFormField(
+          controller: passwordController,
+          hintText: 'password',
+          textInputAction: TextInputAction.done,
+          textInputType: TextInputType.visiblePassword,
+          obscureText: true,
+        ));
+  }
+}
