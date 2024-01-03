61074b61a727eb63516165e943b78667cd03c028
diandrakina <diandrapramestikinasih@gmail.com>
2024-01-03 11:16:07 +0700

    diandra_test

---
+++ lib/styles/styles.dart
@@ -0,0 +1,327 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const black = Colors.black;
+  static const baseColor = Color.fromRGBO(0, 169, 255, 1);
+  static const fourthColor = Color.fromRGBO(205, 245, 253, 1);
+  static const white = Colors.white;
+  static const inactive = Color.fromRGBO(217, 217, 217, 0.4);
+  static const pastelGreenHealth = Color.fromRGBO(9, 249, 134, 1);
+  static const pastelRed = Color.fromRGBO(254, 96, 96, 1);
+  static const successStreak = Color.fromRGBO(255, 159, 70, 1);
+  static const darkModeCard = Color.fromRGBO(57, 57, 57, 1);
+  static const activeCalendar = Color.fromRGBO(95, 95, 95, 1);
+  static const inactiveCalendar = Color.fromRGBO(87, 87, 87, 0.4);
+  static const bgDarkMode = Color.fromRGBO(44, 44, 44, 1);
+  static const floatingGrey = Color.fromRGBO(84, 84, 84, 1);
+  static const starYellow = Color.fromRGBO(223, 162, 6, 1);
+  static const deadBlue = Color.fromRGBO(109, 206, 255, 1);
+  static const pastelPurple = Color.fromRGBO(218, 114, 255, 1);
+  static const bluePowder = Color.fromRGBO(170, 226, 255, 1);
+  static const bluePowderDark = Color.fromRGBO(102, 200, 250, 1);
+  static const bluePowderDarker = Color.fromRGBO(32, 180, 255, 1);
+  static const bluePowderBlack = Color.fromRGBO(2, 153, 231, 1);
+  static const inactiveTask = Color.fromRGBO(74, 74, 74, 1);
+  static const baseColorCard = Color.fromRGBO(160, 233, 255, 1);
+}
+
+class TextStyles {
+  static TextStyle title = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.bold,
+      fontSize: 18.0,
+      color: AppColors.black);
+
+  static TextStyle body = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.normal,
+      fontSize: 16.0,
+      color: AppColors.black);
+
+  static TextStyle medium_16 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.normal,
+      fontSize: 16.0,
+      color: AppColors.black);
+
+  static TextStyle bold_16 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.bold,
+      fontSize: 16.0,
+      color: AppColors.black);
+
+  static TextStyle regular_16 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.normal,
+      fontSize: 16.0,
+      color: AppColors.black);
+
+  static TextStyle light_16 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.w300,
+      fontSize: 16.0,
+      color: AppColors.black);
+
+  static TextStyle thin_16 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.w100,
+      fontSize: 16.0,
+      color: AppColors.black);
+
+  static TextStyle bold_24 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.bold,
+      fontSize: 24.0,
+      color: AppColors.black);
+
+  static TextStyle medium_24 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.normal,
+      fontSize: 24.0,
+      color: AppColors.black);
+
+  static TextStyle regular_24 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.normal,
+      fontSize: 24.0,
+      color: AppColors.black);
+
+  static TextStyle light_24 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.w300,
+      fontSize: 24.0,
+      color: AppColors.black);
+
+  static TextStyle thin_24 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.w100,
+      fontSize: 24.0,
+      color: AppColors.black);
+
+  static TextStyle bold_18 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.bold,
+      fontSize: 18.0,
+      color: AppColors.black);
+
+  static TextStyle medium_18 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.normal,
+      fontSize: 18.0,
+      color: AppColors.black);
+
+  static TextStyle regular_18 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.normal,
+      fontSize: 18.0,
+      color: AppColors.black);
+
+  static TextStyle light_18 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.w300,
+      fontSize: 18.0,
+      color: AppColors.black);
+
+  static TextStyle thin_18 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.w100,
+      fontSize: 18.0,
+      color: AppColors.black);
+
+  static TextStyle bold_14 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.bold,
+      fontSize: 14.0,
+      color: AppColors.black);
+
+  static TextStyle medium_14 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.normal,
+      fontSize: 14.0,
+      color: AppColors.black);
+
+  static TextStyle regular_14 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.normal,
+      fontSize: 14.0,
+      color: AppColors.black);
+
+  static TextStyle light_14 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.w300,
+      fontSize: 14.0,
+      color: AppColors.black);
+
+  static TextStyle thin_14 = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.w100,
+      fontSize: 14.0,
+      color: AppColors.black);
+
+  static TextStyle big_samsung = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.bold,
+      fontSize: 36.0,
+      color: AppColors.black);
+
+  static TextStyle intro_39_bold = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.bold,
+      fontSize: 40.0,
+      color: AppColors.black);
+
+  static TextStyle GR_14_light = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.w300,
+      fontSize: 14.83,
+      color: AppColors.black);
+
+  static TextStyle GR_9_light = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.w300,
+      fontSize: 9.17,
+      color: AppColors.black);
+
+  static TextStyle GR_24_light = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.w300,
+      fontSize: 24.0,
+      color: AppColors.black);
+
+  static TextStyle GR_16_light = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.w300,
+      fontSize: 15.28,
+      color: AppColors.black);
+
+  static TextStyle GR_15_light = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.w300,
+      fontSize: 16.0,
+      color: AppColors.black);
+
+  static TextStyle GR_40_title_light = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.w300,
+      fontSize: 40.0,
+      color: AppColors.black);
+
+  static TextStyle GR_24_title_light = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.w300,
+      fontSize: 24.72,
+      color: AppColors.black);
+
+  static TextStyle GR_15_title_light = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.w300,
+      fontSize: 15.28,
+      color: AppColors.black);
+
+  static TextStyle GR_9_title_light = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.w300,
+      fontSize: 9.44,
+      color: AppColors.black);
+
+  static TextStyle GR_15_title_regular = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.normal,
+      fontSize: 15.28,
+      color: AppColors.black);
+
+  static TextStyle GR_40_title_regular = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.normal,
+      fontSize: 40,
+      color: AppColors.black);
+
+  static TextStyle GR_24_title_regular = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.normal,
+      fontSize: 24.72,
+      color: AppColors.black);
+
+  static TextStyle GR_14_bold = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.bold,
+      fontSize: 14.83,
+      color: AppColors.black);
+
+  static TextStyle GR_15_bold = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.bold,
+      fontSize: 15.28,
+      color: AppColors.black);
+
+  static TextStyle GR_16_bold = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.bold,
+      fontSize: 16.0,
+      color: AppColors.black);
+
+  static TextStyle GR_42_bold = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.bold,
+      fontSize: 42.0,
+      color: AppColors.black);
+
+  static TextStyle GR_24_bold = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.bold,
+      fontSize: 24.72,
+      color: AppColors.black);
+
+  static TextStyle GR_16_regular = const TextStyle(
+      fontFamily: 'SamsungSans',
+      fontWeight: FontWeight.normal,
+      fontSize: 16.0,
+      color: AppColors.black);
+}
+
+class AppDecoration {
+  static BoxDecoration get fill_baseColor =>
+      BoxDecoration(color: AppColors.baseColor);
+  static BoxDecoration get fill_fourthColor =>
+      BoxDecoration(color: AppColors.fourthColor);
+  static BoxDecoration get fill_white => BoxDecoration(color: AppColors.white);
+  static BoxDecoration get fill_inactive =>
+      BoxDecoration(color: AppColors.inactive);
+  static BoxDecoration get fill_pastelGreenHealth =>
+      BoxDecoration(color: AppColors.pastelGreenHealth);
+  static BoxDecoration get fill_pastelRed =>
+      BoxDecoration(color: AppColors.pastelRed);
+  static BoxDecoration get fill_successStreak =>
+      BoxDecoration(color: AppColors.successStreak);
+  static BoxDecoration get fill_darkModeCard =>
+      BoxDecoration(color: AppColors.darkModeCard);
+  static BoxDecoration get fill_activeCalendar =>
+      BoxDecoration(color: AppColors.activeCalendar);
+  static BoxDecoration get fill_inactiveCalendar =>
+      BoxDecoration(color: AppColors.inactiveCalendar);
+  static BoxDecoration get fill_bgDarkMode =>
+      BoxDecoration(color: AppColors.bgDarkMode);
+  static BoxDecoration get fill_floatingGrey =>
+      BoxDecoration(color: AppColors.floatingGrey);
+  static BoxDecoration get fill_starYellow =>
+      BoxDecoration(color: AppColors.starYellow);
+  static BoxDecoration get fill_deadBlue =>
+      BoxDecoration(color: AppColors.deadBlue);
+  static BoxDecoration get fill_pastelPurple =>
+      BoxDecoration(color: AppColors.pastelPurple);
+  static BoxDecoration get fill_bluePowder =>
+      BoxDecoration(color: AppColors.bluePowder);
+  static BoxDecoration get fill_bluePowderDark =>
+      BoxDecoration(color: AppColors.bluePowderDark);
+  static BoxDecoration get fill_bluePowderDarker =>
+      BoxDecoration(color: AppColors.bluePowderDarker);
+  static BoxDecoration get fill_bluePowderBlack =>
+      BoxDecoration(color: AppColors.bluePowderBlack);
+  static BoxDecoration get fill_inactiveTask =>
+      BoxDecoration(color: AppColors.inactiveTask);
+  static BoxDecoration get fill_baseColorCard =>
+      BoxDecoration(color: AppColors.baseColorCard);
+}
+
+
+// PrimaryColors get appTheme => ThemeHelper()
