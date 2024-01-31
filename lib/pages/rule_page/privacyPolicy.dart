b35d0859f485272d3ac6f42c143d6e3cd2856a88
Joeliardo <jleviothniel@gmail.com>
2024-01-31 09:51:30 +0700

    joel privacypolicy

---
+++ lib/pages/rule_page/privacyPolicy.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:selamat_application/styles/styles.dart';
+import 'package:selamat_application/widget/customTextFormField.dart';
+import 'package:selamat_application/widget/customCheckboxButton.dart';
+import 'package:selamat_application/widget/customElevatedButton.dart';
+
+class PrivacyPolicy extends StatelessWidget {
+  const PrivacyPolicy({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
