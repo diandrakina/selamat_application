a0d9c9ad7f53a08446d5d4a790021ba56b66dff0
Joeliardo <jleviothniel@gmail.com>
2024-02-06 16:11:21 +0700

    joel - contact page

--- lib/pages/chat_page/ContactPage.dart
+++ lib/pages/chat_page/ContactPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:selamat_application/models/message_model.dart';
 import 'package:selamat_application/styles/styles.dart';
 import 'package:selamat_application/widget/customTextFormField.dart';
 import 'package:selamat_application/widget/customCheckboxButton.dart';
@@ -17,6 +18,7 @@ class _ContactPageState extends State<ContactPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: AppColors.bgDarkMode,
         leading: IconButton(
           iconSize: 18,
           icon: Icon(
@@ -30,20 +32,74 @@ class _ContactPageState extends State<ContactPage> {
       ),
       body: Column(
         children: [
-          Expanded(
-            child: Container(
-              color: AppColors.darkModeCard,
-              child: TextField(
-                  decoration: InputDecoration(
-                      contentPadding: EdgeInsets.all(12),
-                      hintText: 'Search',
-                      hintStyle: TextStyles.regular_14)),
-            ),
-          ),
+          TextField(
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(12),
+                  hintText: 'Search',
+                  hintStyle: TextStyles.regular_14)),
           Column(
             children: [
               Container(
-                height: 300,
+                height: MediaQuery.of(context).size.height,
+                color: AppColors.bgDarkMode,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30)),
+                  child: ListView.builder(
+                    itemCount: chats.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final Message chat = chats[index];
+                      return Container(
+                        margin:
+                            EdgeInsets.only(top: 5.0, bottom: 5.0, right: 20.0),
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 10.0, vertical: 5.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                CircleAvatar(
+                                  radius: 35.0,
+                                  backgroundImage:
+                                      AssetImage(chat.sender.imageUrl),
+                                ),
+                                SizedBox(width: 10.0),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      chat.sender.name,
+                                      style: TextStyles.medium_18,
+                                    ),
+                                    SizedBox(height: 5.0),
+                                    Container(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.45,
+                                      child: Text(
+                                        chat.text,
+                                        style: TextStyles.light_18,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  chat.time,
+                                  style: TextStyles.thin_18,
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
               ),
             ],
           ),
