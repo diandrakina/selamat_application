0bc0d35a46b0d8121616fbfc67ec47fa27ab5a7b
diandrakina <diandrapramestikinasih@gmail.com>
2024-02-12 08:17:20 +0700

    Main - Betulin

--- lib/pages/rule_page/privacyPolicy.dart
+++ lib/pages/rule_page/privacyPolicy.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:selamat_application/styles/styles.dart';
-import 'package:selamat_application/widget/customTextFormField.dart';
-import 'package:selamat_application/widget/customCheckboxButton.dart';
-import 'package:selamat_application/widget/customElevatedButton.dart';
+import 'package:selamat_application/widget/widget_login_register/customTextFormField.dart';
+import 'package:selamat_application/widget/widget_login_register/customCheckboxButton.dart';
+import 'package:selamat_application/widget/widget_login_register/customElevatedButton.dart';
 
 class PrivacyPolicy extends StatelessWidget {
   const PrivacyPolicy({super.key});
--- lib/pages/welcome_page/youAlmostThere.dart
+++ lib/pages/welcome_page/youAlmostThere.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:selamat_application/styles/styles.dart';
-import 'package:selamat_application/widget/customTextFormField.dart';
-import 'package:selamat_application/widget/customCheckboxButton.dart';
-import 'package:selamat_application/widget/customElevatedButton.dart';
+import 'package:selamat_application/widget/widget_login_register/customTextFormField.dart';
+import 'package:selamat_application/widget/widget_login_register/customCheckboxButton.dart';
+import 'package:selamat_application/widget/widget_login_register/customElevatedButton.dart';
 
 class AlmostThere extends StatelessWidget {
-  const AlmostThere ({super.key});
+  const AlmostThere({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +15,7 @@ class AlmostThere extends StatelessWidget {
           'You Almost\nthere!',
           style: TextStyles.GR_42_bold,
           textAlign: TextAlign.center,
-          ),
+        ),
       ),
     );
   }
